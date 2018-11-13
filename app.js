1d8d427331b7ba5f17dd7606465756dd63d07276
prnjl02 <40962769+prnjl02@users.noreply.github.com>
2018-11-13 17:07:44 +0530

    new change only for fun 13nov18

---
+++ app.js
@@ -0,0 +1,15 @@
+(function(){
+'use strict';
+angular.module("coursera",[])
+       .controller("flipImageCtr",function($scope){
+$scope.stateo="india";
+$scope.function= state (){
+$scope.stateo="map";
+};
+
+       })
+
+}
+
+
+)();
